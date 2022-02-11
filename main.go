d8f0e69699b208622adafe89c58973e131dde9d7
riansyah <mriansyah93@gmail.com>
2022-02-11 09:36:56 +0700

    fix db password

--- main.go
+++ main.go
@@ -4,19 +4,33 @@ import (
 	"api-ranufrozen/handler"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:@tcp(127.0.0.1:3306)/api-ranufrozen?charset=utf8mb4&parseTime=True&loc=Local"
+	errDotenv := godotenv.Load()
+	if errDotenv != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	dsn := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:3306)/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB connect error")
 	}
-	fmt.Println("DB connection succed")
+	fmt.Println("DB connection succeed")
 
 	router := gin.Default()
 
