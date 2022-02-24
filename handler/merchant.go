54b5d2d742c30a918ca6e169fd5e53b78c59a495
riansyah <mriansyah93@gmail.com>
2022-02-24 15:51:02 +0700

    add penjelasan function vs method in go context

--- handler/merchant.go
+++ handler/merchant.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"api-ranufrozen/merchant"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type merchantHandler struct {
+	merchantServ merchant.Service
+}
+
+func NewMerchantHandler(merchServ *merchant.Service) *merchantHandler {
+	return &merchantHandler{merchServ}
+	// func main() call NewMerchantHandler()
+	// NewMerchantHandler() require params 'merchant.Service'
+	// 'merchant.Service' di isi pada struct merchantHandler
+	// merchantHandler digunakan pada method RootHandler dan sebagainya
+	// method vs function: 2 hal yg berbeda pada Go
+}
+
+func (handler *merchantHandler) RootHandler(con *gin.Context) {
+	con.JSON(http.StatusOK, gin.H{
+		"merchants": "merchants",
+	})
+}
