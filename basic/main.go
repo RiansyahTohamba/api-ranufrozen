1356184add9d9ad944e0b481033b641e039f726f
riansyah <mriansyah93@gmail.com>
2022-02-27 08:32:20 +0700

    update example conccurency

--- basic/main.go
+++ basic/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func greetBio(bioChan chan string) {
+	bioChan <- "Bio"
+}
+
+func greetHani(haniChan chan string) {
+	haniChan <- "Hani"
+}
+
 func mapExample() {
 	balance := map[string]float64{
 		"623748280": 100000,
@@ -13,5 +21,10 @@ func mapExample() {
 }
 
 func main() {
-	mapExample()
+	bioChannel := make(chan string)
+	haniChannel := make(chan string)
+	go greetBio(bioChannel)
+	go greetHani(haniChannel)
+	fmt.Println(<-bioChannel)
+	fmt.Println(<-haniChannel)
 }
