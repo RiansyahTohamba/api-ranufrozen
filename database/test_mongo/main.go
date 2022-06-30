e9db46e4b737617af24e91c08a0ed69f3c9c532b
riansyah <mriansyah93@gmail.com>
2022-06-30 17:23:11 +0700

    refactoring since we add new entity named Drink

--- database/test_mongo/main.go
+++ database/test_mongo/main.go
@@ -11,9 +11,27 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var ctx = context.Background()
+func main() {
+	// 1. Create Drink
+	// 1.a. Create Drink 2,3,4
+	// 2. Retrieve Specific Drink
+	// 3. Retrieve All Drink
+	// 4. Update Drink
+	// 4.a Retrieve Specific Drink
+	// 5. Delete Drink 1
+	db, err := connect()
+	if err != nil {
+		log.Println(err)
+	}
+
+	// food example
+	foodRep := NewFoodRepo(db)
+	foodRep.find()
+}
 
 func connect() (*mongo.Database, error) {
+	var ctx = context.Background()
+
 	clOpt := options.Client()
 	clOpt.ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.NewClient(clOpt)
@@ -28,7 +46,47 @@ func connect() (*mongo.Database, error) {
 	return client.Database("ranufrozen"), nil
 }
 
-func find() {
+type drinkRepository struct {
+	db *mongo.Database
+}
+
+func NewDrinkRepo(db *mongo.Database) *drinkRepository {
+	return &drinkRepository{db}
+}
+
+func (dr drinkRepository) find() {
+
+}
+func (dr drinkRepository) insert() {
+	// var ctx = context.Background()
+
+	// db, err := connect()
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// _, err = db.Collection("student").InsertOne(ctx, student{"Wick", 2})
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// _, err = db.Collection("student").InsertOne(ctx, student{"Ethan", 2})
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	fmt.Println("Insert success!")
+}
+
+type foodRepository struct {
+	db *mongo.Database
+}
+
+func NewFoodRepo(db *mongo.Database) *foodRepository {
+	return &foodRepository{db}
+}
+
+func (fr foodRepository) find() {
+	var ctx = context.Background()
+
 	db, err := connect()
 	if err != nil {
 		log.Fatal(err.Error())
@@ -56,10 +114,6 @@ func find() {
 
 }
 
-func main() {
-	find()
-}
-
 type Food struct {
 	Name          string  `bson:"name"`
 	PhotoPath     string  `bson:"photoPath"`
@@ -74,4 +128,8 @@ type Food struct {
 
 	CreatedAt time.Time
 	UpdateAt  time.Time
+}
+
+type Drink struct {
+	Name string `bson:"name"`
 }
