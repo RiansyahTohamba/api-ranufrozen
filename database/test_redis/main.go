a393431b6fc5988d2035c333d93ed24645fb4ae2
riansyah <mriansyah93@gmail.com>
2022-07-04 13:41:04 +0700

    finish string type for redis

--- database/test_redis/main.go
+++ database/test_redis/main.go
@@ -9,21 +9,45 @@ var ctx = context.TODO()
 
 func main() {
 	rdsClient := GetRedisConn()
+	// cartExample(rdsClient)
+	stringExample(rdsClient)
+
+}
+
+func stringExample(rdsClient *RedisClient) {
+	// SET
+	// bagaimana jika SET 2 kali?
+	stRep := NewStringRepo(rdsClient)
+	insertStrRep(stRep)
+	// GET
+	// data terakhir yang akan diambil
+	fmt.Println(stRep.Show("user1"))
+	fmt.Println(stRep.Show("user2"))
+	fmt.Println(stRep.Show("user3"))
+	// lalu jika ada key yang sama pada fitur cart sebelumnya (cart menggunakan key 'user1')
+	// maka akan ditimpa dengan fitur sesudahnya
+
+}
+
+func hashExample(rdsClient *RedisClient) {
+}
+
+func cartExample(rdsClient *RedisClient) {
 	cartRepo := NewCart(rdsClient)
 
-	insertData(*cartRepo)
+	insertCart(*cartRepo)
 	// GET
 	fmt.Println(cartRepo.GetCart(ctx, "user1"))
 	fmt.Println(cartRepo.GetCart(ctx, "user2"))
 
-	removeData(*cartRepo)
+	removeCart(*cartRepo)
 	fmt.Println("after deletion")
 	fmt.Println(cartRepo.GetCart(ctx, "user1"))
 	fmt.Println(cartRepo.GetCart(ctx, "user2"))
 
 }
 
-func insertData(cartRepo CartRepository) {
+func insertCart(cartRepo CartRepository) {
 	// SET
 	// User1 Buy Food
 	cartRepo.AddCart(ctx, "user1", "buah naga")
@@ -35,7 +59,14 @@ func insertData(cartRepo CartRepository) {
 	cartRepo.AddCart(ctx, "user2", "latte tea")
 }
 
-func removeData(cartRepo CartRepository) {
+func insertStrRep(stRep *StringRepo) {
+	stRep.Create("user1", "rendang")
+	stRep.Create("user1", "mie goreng")
+	stRep.Create("user2", "ayam bakar")
+	stRep.Create("user3", "roti panggang")
+}
+
+func removeCart(cartRepo CartRepository) {
 	cartRepo.DeleteCart(ctx, "user1")
 	cartRepo.DeleteCart(ctx, "user2")
 }
